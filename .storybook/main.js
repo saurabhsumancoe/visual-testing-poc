20044c29a0adc93d7a6759c912e2b8b3b6718f19
Saurabh Suman <Saurabh.Suman@connectwise.com>
2025-10-01 17:21:51 +0530

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-docs"
+  ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  }
+};
+export default config;
